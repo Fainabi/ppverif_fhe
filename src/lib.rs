1da79820ac088581b4e028cf666bf037c293e417
Fainabi Arandur <arandurf@gmail.com>
2025-07-06 18:36:34 +0800

    more params

--- src/lib.rs
+++ src/lib.rs
@@ -21,53 +21,83 @@ pub extern "system" fn Java_com_example_ppverif_RustBridge_testClient(
     _class: JClass
 ) -> jfloatArray {
 
-    let mut client = Client::new(DEFAULT_INNER_PRODUCT_PARAMETER, DEFAULT_BLIND_ROTATION_PARAMETER);
-    let pk = client.new_glwe_public_keys_br();
-    let mut server = Server::new(DEFAULT_INNER_PRODUCT_PARAMETER, DEFAULT_BLIND_ROTATION_PARAMETER, pk);
+    let mut rust_floats: Vec<f32> = vec![];
+    let repeat_time = 100;
 
-    // features
-    let mut f1 = vec![0f32; 512];
-    let mut f2 = vec![0f32; 512];
-    f1[1] = 1f32;
-    f2[511] = -0.2f32;
+    for ip_param in [
+        INNER_PRODUCT_PARAMETER_DIMENSION_128,
+        INNER_PRODUCT_PARAMETER_DIMENSION_256,
+        INNER_PRODUCT_PARAMETER_DIMENSION_512,
+        INNER_PRODUCT_PARAMETER_DIMENSION_1024,
+        INNER_PRODUCT_PARAMETER_DIMENSION_2048,
+        INNER_PRODUCT_PARAMETER_DIMENSION_4096,
+    ] {
+        let mut client = Client::new(DEFAULT_INNER_PRODUCT_PARAMETER, DEFAULT_BLIND_ROTATION_PARAMETER);
+        let pk = client.new_glwe_public_keys_br();
+        let mut server = Server::new(DEFAULT_INNER_PRODUCT_PARAMETER, DEFAULT_BLIND_ROTATION_PARAMETER, pk);
 
-    // enrollment
-    let template = client.encrypt_new_template(0, &f1, 512.0);
-    server.enroll(0, template);
-    client.enroll_ggsw_masks(0);
+        // features
+        let mut f1 = vec![0f32; 512];
+        let mut f2 = vec![0f32; 512];
+        f1[1] = 1f32;
+        f2[511] = -0.2f32;
 
-    // verification
-    // Client
-    let now = Instant::now();
-    let query_ct = client.encrypt_glwe(&f2, 512.0);
-    let elapsed = now.elapsed();
-    // println!("Encrypting query ct {} micros", elapsed.as_nanos() as f32 / 1000.0);
-    let encryt_query_ct_time = elapsed.as_nanos() as f32 / 1000.0;
+        // enrollment
+        let template = client.encrypt_new_template(0, &f1, 512.0);
+        server.enroll(0, template);
+        client.enroll_ggsw_masks(0);
 
-    let now = Instant::now();
-    let lut_ct = client.transform_mask_from_database(0, query_ct.as_view()).unwrap();
-    let elapsed = now.elapsed();
-    // println!("Generate lookup table: {} micros", elapsed.as_nanos() as f32 / 1000.0);
-    let generate_lookup_table_time = elapsed.as_nanos() as f32 / 1000.0;
+        // verification
+        let now = Instant::now();
+        for i in 1..repeat_time {
+            client.enroll_ggsw_masks(0);
+        }
+        client.enroll_ggsw_masks(0);
+        let elapsed = now.elapsed();
+        let preprocessing_time = elapsed.as_nanos() as f32 / 1000.0 / (repeat_time as f32);
 
-    // Server
-    let now = Instant::now();
-    let verif_ct = server.verify(0, &query_ct, &lut_ct).unwrap();
-    let elapsed = now.elapsed();
-    // println!("verification time: {} micros", elapsed.as_nanos() as f32 / 1000.0);
-    let verification_time = elapsed.as_nanos() as f32 / 1000.0;
+        // Client
+        let now = Instant::now();
+        for i in 1..repeat_time {
+            let _ = client.encrypt_glwe(&f2, 512.0);
+        }
+        let query_ct = client.encrypt_glwe(&f2, 512.0);
+        let elapsed = now.elapsed();
+        let encryt_query_ct_time = elapsed.as_nanos() as f32 / 1000.0 / (repeat_time as f32);
 
-    // Client
-    let now = Instant::now();
-    let verif_res = client.decrypt_lwe(&verif_ct);
-    let elapsed = now.elapsed();
-    // println!("decrytion time: {} micros", elapsed.as_nanos() as f32 / 1000.0);
-    let decryption_time = elapsed.as_nanos() as f32 / 1000.0;
+        let now = Instant::now();
+        for i in 1..repeat_time {
+            client.transform_mask_to_body_from_database(0, query_ct.as_view());
+        }
+        client.transform_mask_to_body_from_database(0, query_ct.as_view());
+        let elapsed = now.elapsed();
+        let client_innerprod_time = elapsed.as_nanos() as f32 / 1000.0 / (repeat_time as f32);
 
-    // println!("verif res: {}", verif_res);
+        let now = Instant::now();
+        for i in 1..repeat_time {
+            let _ = client.transform_mask_from_database(0, query_ct.as_view()).unwrap();
+        }
+        let lut_ct = client.transform_mask_from_database(0, query_ct.as_view()).unwrap();
+        let elapsed = now.elapsed();
+        let generate_lookup_table_time = elapsed.as_nanos() as f32 / 1000.0 / (repeat_time as f32);
 
-    let rust_floats: Vec<f32> = vec![encryt_query_ct_time, generate_lookup_table_time, verification_time,
-    decryption_time];
+        // Server
+        let verif_ct = server.verify(0, &query_ct, &lut_ct).unwrap();
+
+        // Client
+        let now = Instant::now();
+        for i in 1..repeat_time {
+            let _ = client.decrypt_lwe(&verif_ct);
+        }
+        let verif_res = client.decrypt_lwe(&verif_ct);
+        let elapsed = now.elapsed();
+        let decryption_time = elapsed.as_nanos() as f32 / 1000.0 / (repeat_time as f32);
+
+        vec![preprocessing_time, encryt_query_ct_time, client_innerprod_time, generate_lookup_table_time, decryption_time]
+            .into_iter()
+            .for_each(|t| rust_floats.push(t));
+    }
+    
     let float_array: jni::objects::JPrimitiveArray<'_, f32> = _env.new_float_array(rust_floats.len() as i32).unwrap();
     _env.set_float_array_region(&float_array, 0, &rust_floats).unwrap();
     float_array.into_raw()
--- src/params.rs
+++ src/params.rs
@@ -25,6 +25,68 @@ pub const DEFAULT_INNER_PRODUCT_PARAMETER: GlweParameter<u64> = GlweParameter {
     ciphertext_modulus: 1u128 << 64,
 };
 
+pub const INNER_PRODUCT_PARAMETER_DIMENSION_128: GlweParameter<u64> = GlweParameter {
+    glwe_size: GlweSize(1 + 20),
+    polynomial_size: PolynomialSize(128),
+    std_dev: 1.7347234759768072e-19,  // 3.2 / (2 ** 64)
+    plaintext_modulus: 1 << 19,
+    delta: 1 << 45,
+    decomposition_base_log: DecompositionBaseLog(32),
+    decomposition_level_count: DecompositionLevelCount(1),
+    ciphertext_mask: 0xFFFFFFFF_FFFFFFFF,
+    ciphertext_modulus: 1u128 << 64,
+};
+
+pub const INNER_PRODUCT_PARAMETER_DIMENSION_256: GlweParameter<u64> = GlweParameter {
+    glwe_size: GlweSize(1 + 10),
+    polynomial_size: PolynomialSize(256),
+    std_dev: 1.7347234759768072e-19,  // 3.2 / (2 ** 64)
+    plaintext_modulus: 1 << 19,
+    delta: 1 << 45,
+    decomposition_base_log: DecompositionBaseLog(32),
+    decomposition_level_count: DecompositionLevelCount(1),
+    ciphertext_mask: 0xFFFFFFFF_FFFFFFFF,
+    ciphertext_modulus: 1u128 << 64,
+};
+
+pub const INNER_PRODUCT_PARAMETER_DIMENSION_512: GlweParameter<u64> = DEFAULT_INNER_PRODUCT_PARAMETER;
+
+pub const INNER_PRODUCT_PARAMETER_DIMENSION_1024: GlweParameter<u64> = GlweParameter {
+    glwe_size: GlweSize(1 + 3),
+    polynomial_size: PolynomialSize(1024),
+    std_dev: 1.7347234759768072e-19,  // 3.2 / (2 ** 64)
+    plaintext_modulus: 1 << 19,
+    delta: 1 << 45,
+    decomposition_base_log: DecompositionBaseLog(32),
+    decomposition_level_count: DecompositionLevelCount(1),
+    ciphertext_mask: 0xFFFFFFFF_FFFFFFFF,
+    ciphertext_modulus: 1u128 << 64,
+};
+
+pub const INNER_PRODUCT_PARAMETER_DIMENSION_2048: GlweParameter<u64> = GlweParameter {
+    glwe_size: GlweSize(1 + 2),
+    polynomial_size: PolynomialSize(2048),
+    std_dev: 1.7347234759768072e-19,  // 3.2 / (2 ** 64)
+    plaintext_modulus: 1 << 19,
+    delta: 1 << 45,
+    decomposition_base_log: DecompositionBaseLog(32),
+    decomposition_level_count: DecompositionLevelCount(1),
+    ciphertext_mask: 0xFFFFFFFF_FFFFFFFF,
+    ciphertext_modulus: 1u128 << 64,
+};
+
+pub const INNER_PRODUCT_PARAMETER_DIMENSION_4096: GlweParameter<u64> = GlweParameter {
+    glwe_size: GlweSize(1 + 1),
+    polynomial_size: PolynomialSize(4096),
+    std_dev: 1.7347234759768072e-19,  // 3.2 / (2 ** 64)
+    plaintext_modulus: 1 << 19,
+    delta: 1 << 45,
+    decomposition_base_log: DecompositionBaseLog(32),
+    decomposition_level_count: DecompositionLevelCount(1),
+    ciphertext_mask: 0xFFFFFFFF_FFFFFFFF,
+    ciphertext_modulus: 1u128 << 64,
+};
+
 pub const DEFAULT_BLIND_ROTATION_PARAMETER: GlweParameter<u16> = GlweParameter {
     glwe_size: GlweSize(4),
     polynomial_size: PolynomialSize(256),
